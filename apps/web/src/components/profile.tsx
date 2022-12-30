67c9003d87df945d2a5de5b56614acd8dc4b5a19
Ken Powers <ken@lets.church>
2022-12-29 21:47:51 -0600

    Remove unnecessary wrapping div

--- apps/web/src/components/profile.tsx
+++ apps/web/src/components/profile.tsx
@@ -51,41 +51,35 @@ export default function Profile(props: Props) {
   return (
     <div class="relative ml-4 flex-shrink-0">
       <div>
-        <div>
-          <Show
-            when={props.me}
-            fallback={
-              <A
-                href="/auth/login"
-                class="ml-6 inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              >
-                Login
-              </A>
-            }
-          >
-            <button
-              type="button"
-              class="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              id={menuButtonId}
-              aria-expanded="false"
-              aria-haspopup="true"
-              ref={setReference}
-              onClick={toggleMenu}
+        <Show
+          when={props.me}
+          fallback={
+            <A
+              href="/auth/login"
+              class="ml-6 inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
             >
-              <span class="sr-only">Open user menu</span>
-              <img
-                class="h-8 w-8 rounded-full"
-                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                alt="User Icon"
-              />
-            </button>
-            <Form
-              id={logoutFormId}
-              class="hidden"
-              onSubmit={() => closeMenu()}
+              Login
+            </A>
+          }
+        >
+          <button
+            type="button"
+            class="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            id={menuButtonId}
+            aria-expanded="false"
+            aria-haspopup="true"
+            ref={setReference}
+            onClick={toggleMenu}
+          >
+            <span class="sr-only">Open user menu</span>
+            <img
+              class="h-8 w-8 rounded-full"
+              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt="User Icon"
             />
-          </Show>
-        </div>
+          </button>
+          <Form id={logoutFormId} class="hidden" onSubmit={() => closeMenu()} />
+        </Show>
       </div>
       <FloatingMenu
         ref={setFloating}
