72f46b1b8a6ca1cd1d1871689e3232c706d1fbcc
Ken Powers <ken@lets.church>
2023-04-25 20:46:54 -0500

    More space on homepage

--- apps/web/src/routes/(root)/(watch).tsx
+++ apps/web/src/routes/(root)/(watch).tsx
@@ -86,7 +86,7 @@ export default function WatchRoute() {
       <Show
         when={(data()?.subscriptionUploads?.edges.length ?? 0) > 0}
         fallback={
-          <div class="relative block w-full space-y-5 rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+          <div class="relative mb-6 block w-full space-y-5 rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
             <SubscribeIcon class="mx-auto h-12 w-12 text-gray-400" />
             <span class="mt-2 block text-sm font-semibold text-gray-900">
               There aren't any videos to show you from your subscriptions. Check
