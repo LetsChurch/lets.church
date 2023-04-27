c61f5ffb54107f89b127b978beba92b057f4e181
Ken Powers <ken@lets.church>
2023-04-26 20:19:13 -0500

    Add favicons

--- apps/web/src/root.tsx
+++ apps/web/src/root.tsx
@@ -21,6 +21,25 @@ export default function Root() {
         <Title>Let's Church</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="shortcut icon" href="/favicon.svg" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <Body>
         <Suspense>
