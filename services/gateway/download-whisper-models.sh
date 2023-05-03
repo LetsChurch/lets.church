e8f518574770bba431a645ac82d262602db7f366
Ken Powers <ken@lets.church>
2023-05-02 20:03:51 -0500

    Download whisper models over HTTP instead of git

--- services/gateway/download-whisper-models.sh
+++ services/gateway/download-whisper-models.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
 downloadModel() {
-  mkdir -p /opt/whisper/models/$1
-  cd /opt/whisper/models/$1
-  git init
-  git remote add origin https://huggingface.co/guillaumekln/faster-whisper-$1
-  git fetch origin $2
-  git reset --hard FETCH_HEAD
+  mkdir -p /opt/whisper/models/
+  curl https://data.letschurch.cloud/whisper-ctranslate2/models/$1.tar.gz | tar -xzC /opt/whisper/models/
 }
 
-downloadModel tiny.en 7d45cf02c1ed72d240c0dbf99d544d19bef1b5a3
-downloadModel large-v2 fecb99cc227a240ccd295d99b6c9026e7a179508
+downloadModel tiny.en
+downloadModel large-v2
