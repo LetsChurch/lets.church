641a31f845d4eb2c29bfdb79f78306aad926e16a
Ken Powers <ken@lets.church>
2023-05-04 21:42:52 -0500

    Fill in about and support pages.

--- apps/web/src/routes/(root)/about/(about).tsx
+++ apps/web/src/routes/(root)/about/(about).tsx
@@ -1,3 +1,45 @@
+import A from '~/components/content/a';
+import H1 from '~/components/content/h1';
+import H2 from '~/components/content/h2';
+import P from '~/components/content/p';
+import ExternalLink from '~/components/external-link';
+
 export default function AboutRoute() {
-  return <h1>About!</h1>;
+  return (
+    <div class="bg-white px-6 py-3 lg:px-8">
+      <div class="mx-auto max-w-3xl text-base leading-7 text-gray-700">
+        <H1>All About Let's Church</H1>
+        <P>
+          Let's Church is a non-profit organization (501c3 pending) dedicated to
+          providing free technology resources to Churches and Christian
+          Ministries. Our flagship offering is the site you are looking at right
+          now: sermon and podcast hosting with fully-searchable transcripts 100%
+          free of charge with no ads.
+        </P>
+        <H2 id="accounts-and-membership">The Public Domain</H2>
+        <P>
+          Not only is Let’s Church free to use, but{' '}
+          <ExternalLink href="https://gitlab.com/letschurch/lets.church">
+            our code
+          </ExternalLink>{' '}
+          is also available and dedicated to the public domain. As Jesus said,
+          "freely you received, freely give" (Matt. 10:8). Our philosophy behind
+          this approach is best captured in the book{' '}
+          <ExternalLink href="https://thedoreanprinciple.org/">
+            The Dorean Principle
+          </ExternalLink>
+          .
+        </P>
+        <H2 id="accounts-and-membership">No Ads</H2>
+        <P>
+          Let's Church, as a company, will never run ads on principle. We rely
+          entirely on <A href="/support">support</A> from our users.
+        </P>
+        <h2 class="mt-36 flex flex-col space-y-5 text-center text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          <small>Ready?</small>
+          <strong class="text-5xl font-bold">Let's Church</strong>
+        </h2>
+      </div>
+    </div>
+  );
 }
--- apps/web/src/routes/(root)/support.tsx
+++ apps/web/src/routes/(root)/support.tsx
@@ -1,3 +1,40 @@
+import A from '~/components/content/a';
+import H1 from '~/components/content/h1';
+import H2 from '~/components/content/h2';
+import P from '~/components/content/p';
+
 export default function SupportRoute() {
-  return <h1>Support!</h1>;
+  return (
+    <div class="bg-white px-6 py-3 lg:px-8">
+      <div class="mx-auto max-w-3xl text-base leading-7 text-gray-700">
+        <H1>Support Let's Church</H1>
+        <P>
+          <A href="/about">
+            Let's Church operates as a non-profit organization
+          </A>{' '}
+          (501c3 pending). We operate completely free of charge with no ads. We
+          plan on accepting donations through our website soon. In the iterim,
+          if you would like to support Let's Church feel free to reach out to{' '}
+          <A href="&#109;&#097;&#105;&#108;&#116;&#111;&#058;&#99;&#111;n&#116;&#97;&#99;&#116;&#64;l&#101;t&#115;&#46;&#99;h&#117;&#114;c&#104;">
+            cont&#97;ct&#64;&#108;&#101;t&#115;&#46;&#99;h&#117;r&#99;h
+          </A>
+          .
+        </P>
+        <P>
+          Please do not let any giving to Let's Church interfere with giving to
+          your local church. It is important that you first support your local
+          community of believers before considering how much you can give to
+          Let's Church.
+        </P>
+        <H2>Spread the Word</H2>
+        <P>
+          Another way you can support Let's Church is by spreading the word.
+          Share our website, share our social media, share sermons, videos, and
+          podcasts that have been helpful or edifying to you. If you know of any
+          churches or ministires in need of free sermon or media hosting, send
+          them our way!
+        </P>
+      </div>
+    </div>
+  );
 }
