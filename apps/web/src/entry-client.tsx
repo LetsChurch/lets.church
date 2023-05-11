035dd19ea7d8b12bbcafbbef1592ac88a22cc3f5
Ken Powers <ken@lets.church>
2023-05-11 17:01:28 -0500

    Add plausible

--- apps/web/src/entry-client.tsx
+++ apps/web/src/entry-client.tsx
@@ -1,3 +1,11 @@
 import { mount, StartClient } from 'solid-start/entry-client';
+import Plausible from 'plausible-tracker';
+
+const plausible = Plausible({
+  domain: 'lets.church',
+});
+
+plausible.enableAutoPageviews();
+plausible.enableAutoOutboundTracking();
 
 mount(() => <StartClient />, document);
