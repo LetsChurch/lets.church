0a5856965d33c24712973152be0e56d4e7998c5b
Ken Powers <ken@lets.church>
2023-05-22 17:05:33 -0500

    Simplify logic for actions which upload assets

--- services/gateway/src/temporal/activities/transcode/create-thumbnails.ts
+++ services/gateway/src/temporal/activities/transcode/create-thumbnails.ts
@@ -2,7 +2,6 @@ import { basename, join } from 'node:path';
 import { stat } from 'node:fs/promises';
 import mkdirp from 'mkdirp';
 import { Context } from '@temporalio/activity';
-import PQueue from 'p-queue';
 import pMap from 'p-map';
 import fastGlob from 'fast-glob';
 import { chunk, compact, maxBy, throttle } from 'lodash-es';
@@ -29,11 +28,11 @@ export default async function createThumbnails(
 
   Context.current().heartbeat();
   try {
+    console.log('Creating thumbnails with ffmpeg');
     const proc = runFfmpegThumbnails(dir, downloadPath, cancellationSignal);
     proc.stdout?.on('data', dataHeartbeat);
     proc.stderr?.on('data', dataHeartbeat);
     await proc;
-    const uploadQueue = new PQueue({ concurrency: 1 });
     const thumbnailJpgs = (await fastGlob(join(dir, '*.jpg'))).sort();
     const thumbnailsWithSizes = await pMap(
       thumbnailJpgs,
@@ -48,70 +47,58 @@ export default async function createThumbnails(
     const largestThumbnail = maxBy(thumbnailsWithSizes, 'size')?.path;
     if (largestThumbnail) {
       // Upload the largest thumbnail as the video thumbnail
-      uploadQueue.add(
-        async () => {
-          Context.current().heartbeat();
-          console.log(`Uploading thumbnail: ${largestThumbnail}`);
-          const key = `${uploadRecordId}/${basename(largestThumbnail)}`;
-          await retryablePutFile({
-            to: 'PUBLIC',
-            key,
-            contentType: 'image/jpeg',
-            path: largestThumbnail,
-            contentLength: (await stat(largestThumbnail)).size,
+      Context.current().heartbeat();
+      console.log(`Uploading thumbnail: ${largestThumbnail}`);
+      const key = `${uploadRecordId}/${basename(largestThumbnail)}`;
+      await retryablePutFile({
+        to: 'PUBLIC',
+        key,
+        contentType: 'image/jpeg',
+        path: largestThumbnail,
+        contentLength: (await stat(largestThumbnail)).size,
+      });
+      await pRetry(
+        async (attempt) => {
+          console.log(`Setting thumbnail path: attempt ${attempt}`);
+
+          const blurhash = await imageToBlurhash(largestThumbnail);
+
+          await updateUploadRecord(uploadRecordId, {
+            defaultThumbnailPath: key,
+            thumbnailBlurhash: blurhash,
           });
-          await pRetry(
-            async (attempt) => {
-              console.log(`Setting thumbnail path: attempt ${attempt}`);
-
-              const blurhash = await imageToBlurhash(largestThumbnail);
-
-              await updateUploadRecord(uploadRecordId, {
-                defaultThumbnailPath: key,
-                thumbnailBlurhash: blurhash,
-              });
-            },
-            {
-              retries: 5,
-              onFailedAttempt: (e) => {
-                console.log(
-                  `${e.attemptNumber}: Failed to set thumbnail path: ${e}`,
-                );
-              },
-            },
-          );
-          Context.current().heartbeat();
-          console.log(`Done uploading thumbnail: ${largestThumbnail}`);
         },
         {
-          signal: cancellationSignal,
+          retries: 5,
+          onFailedAttempt: (e) => {
+            console.log(
+              `${e.attemptNumber}: Failed to set thumbnail path: ${e}`,
+            );
+          },
         },
       );
+      Context.current().heartbeat();
+      console.log(`Done uploading thumbnail: ${largestThumbnail}`);
     }
     // Upload the remaining thumbnails
-    uploadQueue.addAll(
-      thumbnailJpgs.map((path) => async () => {
-        // Skip uploading the largest thumbnail here
-        if (path === largestThumbnail) {
-          return;
-        }
+    for (const path of thumbnailJpgs) {
+      // Skip uploading the largest thumbnail here
+      if (path === largestThumbnail) {
+        return;
+      }
 
-        Context.current().heartbeat();
-        console.log(`Uploading thumbnail: ${path}`);
-        await retryablePutFile({
-          to: 'PUBLIC',
-          key: `${uploadRecordId}/${basename(path)}`,
-          contentType: 'image/jpeg',
-          path,
-          contentLength: (await stat(path)).size,
-        });
-        Context.current().heartbeat();
-        console.log(`Done uploading thumbnail: ${path}`);
-      }),
-      {
-        signal: cancellationSignal,
-      },
-    );
+      Context.current().heartbeat();
+      console.log(`Uploading thumbnail: ${path}`);
+      await retryablePutFile({
+        to: 'PUBLIC',
+        key: `${uploadRecordId}/${basename(path)}`,
+        contentType: 'image/jpeg',
+        path,
+        contentLength: (await stat(path)).size,
+      });
+      Context.current().heartbeat();
+      console.log(`Done uploading thumbnail: ${path}`);
+    }
     const chunkSize = Math.ceil(thumbnailsWithSizes.length / 5);
     const thumbnailsWithSizesChunks = chunk(thumbnailsWithSizes, chunkSize);
     const pickedThumbnails = compact(
@@ -120,23 +107,15 @@ export default async function createThumbnails(
     Context.current().heartbeat();
     console.log({ pickedThumbnails });
     await concatThumbs(dir, pickedThumbnails);
-    uploadQueue.add(
-      async () => {
-        console.log('Uploading hovernail');
-        await retryablePutFile({
-          to: 'PUBLIC',
-          key: `${uploadRecordId}/hovernail.jpg`,
-          contentType: 'image/jpeg',
-          path: join(dir, 'hovernail.jpg'),
-        });
-        Context.current().heartbeat();
-        console.log('Done uploading hovernail');
-      },
-      {
-        signal: cancellationSignal,
-      },
-    );
-    await uploadQueue.onEmpty();
+    console.log('Uploading hovernail');
+    await retryablePutFile({
+      to: 'PUBLIC',
+      key: `${uploadRecordId}/hovernail.jpg`,
+      contentType: 'image/jpeg',
+      path: join(dir, 'hovernail.jpg'),
+    });
+    Context.current().heartbeat();
+    console.log('Done uploading hovernail');
   } catch (e) {
     console.log('Error!');
     console.log(e);
--- services/gateway/src/temporal/activities/transcode/transcode.ts
+++ services/gateway/src/temporal/activities/transcode/transcode.ts
@@ -2,8 +2,6 @@ import { basename, join } from 'node:path';
 import { unlink, stat } from 'node:fs/promises';
 import { Context } from '@temporalio/activity';
 import mkdirp from 'mkdirp';
-import PQueue from 'p-queue';
-import chokidar from 'chokidar';
 import fastGlob from 'fast-glob';
 import { throttle } from 'lodash-es';
 import rimraf from 'rimraf';
@@ -35,7 +33,6 @@ export default async function transcode(
   Context.current().heartbeat('job start');
   const cancellationSignal = Context.current().cancellationSignal;
   const dir = join(WORK_DIR, uploadRecordId);
-  const uploadQueue = new PQueue({ concurrency: 1 });
   const dataHeartbeat = throttle(() => Context.current().heartbeat(), 5000);
   const throttledUpdateUploadRecord = throttle(updateUploadRecord, 2500);
 
@@ -83,40 +80,6 @@ export default async function transcode(
       `Will encode ${variants.length} variants: ${formatter.format(variants)}`,
     );
 
-    // Watch for new video segments and upload them
-    console.log('Setting up file watcher');
-    const watcher = chokidar.watch(join(dir, '*.ts')).on('add', (path) => {
-      console.log(`New media segment: ${path}`);
-
-      const fileName = basename(path);
-
-      uploadQueue.add(
-        async () => {
-          Context.current().heartbeat(`Starting uplaod: ${path}`);
-          console.log(`Uploading media segment: ${path}`);
-
-          await retryablePutFile({
-            to: 'PUBLIC',
-            key: `${uploadRecordId}/${fileName}`,
-            contentType: 'video/mp2ts',
-            contentLength: (await stat(path)).size,
-            path,
-          });
-
-          console.log(`Done uploading media segment: ${path}`);
-          console.log(`Deleting ${path}`);
-
-          await unlink(path);
-
-          console.log(`Deleted ${path}`);
-          Context.current().heartbeat(`Uploading done: ${path}`);
-        },
-        {
-          signal: cancellationSignal,
-        },
-      );
-    });
-
     console.log('Running ffmpeg');
 
     const encodeProc = runFfmpegEncode(
@@ -152,6 +115,29 @@ export default async function transcode(
 
     console.log(`ffmpeg finished with code ${encodeProcRes.exitCode}`);
 
+    const segmentFiles = await fastGlob(join(dir, '*.ts'));
+
+    for (const path of segmentFiles) {
+      Context.current().heartbeat(`Starting uplaod: ${path}`);
+      console.log(`Uploading media segment: ${path}`);
+
+      await retryablePutFile({
+        to: 'PUBLIC',
+        key: `${uploadRecordId}/${basename(path)}`,
+        contentType: 'video/mp2ts',
+        contentLength: (await stat(path)).size,
+        path,
+      });
+
+      console.log(`Done uploading media segment: ${path}`);
+      console.log(`Deleting ${path}`);
+
+      await unlink(path);
+
+      console.log(`Deleted ${path}`);
+      Context.current().heartbeat(`Uploading done: ${path}`);
+    }
+
     console.log('Cancelling remaining upload record updates');
 
     throttledUpdateUploadRecord.cancel();
@@ -168,93 +154,74 @@ export default async function transcode(
     );
 
     // Upload downloadable files
-    console.log(
-      `Queueing ${downloadableFiles.length} downloadable files for upload`,
-    );
-    uploadQueue.addAll(
-      downloadableFiles.map((path) => async () => {
-        const filename = basename(path);
-        const contentType = mime.getType(filename);
-        invariant(contentType !== null, 'Mime type should not be null');
-        Context.current().heartbeat(`Uploading downloadable file`);
-        console.log(`Uploading downloadable file: ${filename}`);
-        const byteSize = (await stat(path)).size;
-        await retryablePutFile({
-          to: 'PUBLIC',
-          key: `${uploadRecordId}/${filename}`,
-          contentType,
-          path,
-          contentLength: byteSize,
-        });
-        Context.current().heartbeat(`Uploaded downloadable file: ${filename}`);
-        console.log(`Uploaded downloadable file: ${filename}`);
-        console.log('Recording download size');
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore: TODO: type safety
-        const variant: UploadVariant = filename.split('.')[0];
-        invariant(variant, 'variant should be defined');
-        await recordDownloadSize(uploadRecordId, variant, byteSize);
-      }),
-      {
-        signal: cancellationSignal,
-      },
-    );
+    console.log(`Uploading ${downloadableFiles.length} downloadable files`);
+    for (const path of downloadableFiles) {
+      const filename = basename(path);
+      const contentType = mime.getType(filename);
+      invariant(contentType !== null, 'Mime type should not be null');
+      Context.current().heartbeat(`Uploading downloadable file`);
+      console.log(`Uploading downloadable file: ${filename}`);
+      const byteSize = (await stat(path)).size;
+      await retryablePutFile({
+        to: 'PUBLIC',
+        key: `${uploadRecordId}/${filename}`,
+        contentType,
+        path,
+        contentLength: byteSize,
+      });
+      Context.current().heartbeat(`Uploaded downloadable file: ${filename}`);
+      console.log(`Uploaded downloadable file: ${filename}`);
+      console.log('Recording download size');
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore: TODO: type safety
+      const variant: UploadVariant = filename.split('.')[0];
+      invariant(variant, 'variant should be defined');
+      await recordDownloadSize(uploadRecordId, variant, byteSize);
+    }
 
     console.log('Finding playlist files');
 
     const playlists = await fastGlob(join(dir, '*.m3u8'));
 
     console.log(`Found playlist files:\n - ${playlists.join('\n -')}`);
-    console.log(`Queueing ${playlists.length} playlist files for upload`);
+    console.log(`Uploading ${playlists.length} playlist files`);
 
     // Upload playlist files
-    uploadQueue.addAll(
-      playlists.map((path) => async () => {
-        const filename = basename(path);
-        Context.current().heartbeat(`Uploading playlist file`);
-        console.log(`Uploading playlist file: ${filename}`);
-        await retryablePutFile({
-          to: 'PUBLIC',
-          key: `${uploadRecordId}/${filename}`,
-          contentType: 'application/x-mpegURL',
-          path,
-          contentLength: (await stat(path)).size,
-        });
-        Context.current().heartbeat(`Uploaded playlist file: ${filename}`);
-        console.log(`Uploaded playlist file: ${filename}`);
-      }),
-      {
-        signal: cancellationSignal,
-      },
-    );
+    for (const path of playlists) {
+      const filename = basename(path);
+      Context.current().heartbeat(`Uploading playlist file`);
+      console.log(`Uploading playlist file: ${filename}`);
+      await retryablePutFile({
+        to: 'PUBLIC',
+        key: `${uploadRecordId}/${filename}`,
+        contentType: 'application/x-mpegURL',
+        path,
+        contentLength: (await stat(path)).size,
+      });
+      Context.current().heartbeat(`Uploaded playlist file: ${filename}`);
+      console.log(`Uploaded playlist file: ${filename}`);
+    }
 
     // Upload master playlist if there is more than just audio
     if (variants.some((v) => v.startsWith('VIDEO'))) {
       console.log(
-        `Queueing upload of master playlist file given the variants: ${formatter.format(
+        `Uploading master playlist file given the variants: ${formatter.format(
           variants,
         )}`,
       );
-      uploadQueue.add(
-        async () => {
-          console.log('Uploading master playlist file');
-          Context.current().heartbeat(`Uploading playlist file`);
-          const playlistBuffer = Buffer.from(
-            variantsToMasterVideoPlaylist(variants),
-          );
-          await retryablePutFile({
-            to: 'PUBLIC',
-            key: `${uploadRecordId}/master.m3u8`,
-            contentType: 'application/x-mpegURL',
-            body: playlistBuffer,
-          });
-          Context.current().heartbeat('Uploaded master playlist file');
-          console.log('Uploaded master playlist file');
-        },
-        {
-          signal: cancellationSignal,
-        },
+      console.log('Uploading master playlist file');
+      Context.current().heartbeat(`Uploading playlist file`);
+      const playlistBuffer = Buffer.from(
+        variantsToMasterVideoPlaylist(variants),
       );
+      await retryablePutFile({
+        to: 'PUBLIC',
+        key: `${uploadRecordId}/master.m3u8`,
+        contentType: 'application/x-mpegURL',
+        body: playlistBuffer,
+      });
+      Context.current().heartbeat('Uploaded master playlist file');
+      console.log('Uploaded master playlist file');
     } else {
       console.log(
         `Not creating master playlist given the variants: ${formatter.format(
@@ -272,57 +239,39 @@ export default async function transcode(
     );
 
     console.log('Queuing upload of peaks');
-    uploadQueue.addAll([
-      async () => {
-        console.log('Uploading peak json');
-        Context.current().heartbeat(`Uploading peak json`);
-        await retryablePutFile({
-          to: 'PUBLIC',
-          key: `${uploadRecordId}/peaks.json`,
-          contentType: 'application/json',
-          path: peakFiles.json,
-          contentLength: (await stat(peakFiles.json)).size,
-        });
-        Context.current().heartbeat('Uploaded peak json');
-        console.log('Uploaded peak json');
-      },
-      async () => {
-        console.log('Uploading peak dat');
-        Context.current().heartbeat(`Uploading peak dat`);
-        await retryablePutFile({
-          to: 'PUBLIC',
-          key: `${uploadRecordId}/peaks.dat`,
-          contentType: 'application/octet-stream',
-          path: peakFiles.dat,
-          contentLength: (await stat(peakFiles.dat)).size,
-        });
-        Context.current().heartbeat('Uploaded peak dat');
-        console.log('Uploaded peak dat');
-      },
-    ]);
+    console.log('Uploading peak json');
+    Context.current().heartbeat(`Uploading peak json`);
+    await retryablePutFile({
+      to: 'PUBLIC',
+      key: `${uploadRecordId}/peaks.json`,
+      contentType: 'application/json',
+      path: peakFiles.json,
+      contentLength: (await stat(peakFiles.json)).size,
+    });
+    Context.current().heartbeat('Uploaded peak json');
+    console.log('Uploaded peak json');
+    console.log('Uploading peak dat');
+    Context.current().heartbeat(`Uploading peak dat`);
+    await retryablePutFile({
+      to: 'PUBLIC',
+      key: `${uploadRecordId}/peaks.dat`,
+      contentType: 'application/octet-stream',
+      path: peakFiles.dat,
+      contentLength: (await stat(peakFiles.dat)).size,
+    });
+    Context.current().heartbeat('Uploaded peak dat');
+    console.log('Uploaded peak dat');
 
     // Upload logs
     console.log('Queing upload of logs');
-    uploadQueue.add(
-      async () => {
-        Context.current().heartbeat('Uploading stdout');
-        await retryablePutFile({
-          to: 'INGEST',
-          key: `${uploadRecordId}/stdout.txt`,
-          contentType: 'text/plain',
-          body: Buffer.from(encodeProcRes.stdout),
-        });
-        Context.current().heartbeat('Uploaded stdout');
-      },
-      {
-        signal: cancellationSignal,
-      },
-    );
-
-    console.log('Waiting for upload queue to be idle');
-    await uploadQueue.onIdle();
-    console.log('Waiting for file watcher to close');
-    await watcher.close();
+    Context.current().heartbeat('Uploading stdout');
+    await retryablePutFile({
+      to: 'INGEST',
+      key: `${uploadRecordId}/stdout.txt`,
+      contentType: 'text/plain',
+      body: Buffer.from(encodeProcRes.stdout),
+    });
+    Context.current().heartbeat('Uploaded stdout');
     console.log('Queueing final update for upload record');
     await updateUploadRecord(uploadRecordId, {
       variants,
