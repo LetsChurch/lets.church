a8258ba1b7523770cfa23e1090999cb1f8488102
Ken Powers <ken@lets.church>
2023-05-22 21:54:49 -0500

    Download whisper models separately to attempt to utilize caching better

--- services/gateway/download-whisper-models.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-downloadModel() {
-  mkdir -p /opt/whisper/models/
-  curl https://data.letschurch.cloud/whisper-ctranslate2/models/$1.tar.gz | tar -xzC /opt/whisper/models/
-}
-
-downloadModel tiny.en
-downloadModel large-v2
