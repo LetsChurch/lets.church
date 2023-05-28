8f7b73bc5c1dc84a0a8ef98352bd6c877e9e0194
Ken Powers <ken@lets.church>
2023-05-27 21:33:52 -0500

    Mark upload as private while deleting

--- services/gateway/src/temporal/activities/background/delete-upload-record.ts
+++ services/gateway/src/temporal/activities/background/delete-upload-record.ts
@@ -3,6 +3,22 @@ import prisma from '../../../util/prisma';
 import { client as esClient } from '../../../util/elasticsearch';
 import { deletePrefix } from '../../../util/s3';
 
+export async function markUploadPrivate(id: string) {
+  console.log(`Marking upload record ${id} as private`);
+
+  try {
+    await prisma.uploadRecord.update({
+      where: { id },
+      data: { visibility: 'PRIVATE' },
+    });
+  } catch (e) {
+    console.log(`Error marking upload record ${id} as private: ${e}`);
+    return false;
+  }
+
+  return true;
+}
+
 export async function deleteUploadRecordSearch(id: string) {
   console.log(`Deleting upload record search entry for ${id}`);
 
--- services/gateway/src/temporal/workflows/delete-upload.ts
+++ services/gateway/src/temporal/workflows/delete-upload.ts
@@ -2,14 +2,13 @@ import { proxyActivities, defineSignal } from '@temporalio/workflow';
 import type * as activities from '../activities/background';
 import { BACKGROUND_QUEUE } from '../queues';
 
-const { deleteUploadRecordDb, deleteUploadRecordSearch } = proxyActivities<
-  typeof activities
->({
-  startToCloseTimeout: '1 minute',
-  heartbeatTimeout: '1 minute',
-  taskQueue: BACKGROUND_QUEUE,
-  retry: { maximumAttempts: 5 },
-});
+const { deleteUploadRecordDb, deleteUploadRecordSearch, markUploadPrivate } =
+  proxyActivities<typeof activities>({
+    startToCloseTimeout: '1 minute',
+    heartbeatTimeout: '1 minute',
+    taskQueue: BACKGROUND_QUEUE,
+    retry: { maximumAttempts: 5 },
+  });
 
 const { deleteUploadRecordS3Objects } = proxyActivities<typeof activities>({
   startToCloseTimeout: '10 minute',
@@ -22,6 +21,7 @@ export const uploadDoneSignal =
   defineSignal<[Array<string>, string]>('uploadDone');
 
 export async function deleteUploadWorkflow(uploadRecordId: string) {
+  await markUploadPrivate(uploadRecordId);
   await deleteUploadRecordSearch(uploadRecordId);
   await deleteUploadRecordS3Objects(uploadRecordId);
   await deleteUploadRecordDb(uploadRecordId);
