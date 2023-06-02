b8bed468bbd2836dc81192ab17f24036f89236f4
Ken Powers <ken@lets.church>
2023-06-01 21:58:35 -0500

    Fix generatePeaks workflow

--- services/gateway/src/temporal/workflows/generate-peaks.ts
+++ services/gateway/src/temporal/workflows/generate-peaks.ts
@@ -1,11 +1,11 @@
 import { proxyActivities } from '@temporalio/workflow';
 import type * as transcodeActivities from '../activities/transcode';
-import { BACKGROUND_QUEUE } from '../queues';
+import { TRANSCODE_QUEUE } from '../queues';
 
 const { generatePeaks } = proxyActivities<typeof transcodeActivities>({
   startToCloseTimeout: '10 minutes',
   heartbeatTimeout: '1 minute',
-  taskQueue: BACKGROUND_QUEUE,
+  taskQueue: TRANSCODE_QUEUE,
   retry: { maximumAttempts: 2 },
 });
 
