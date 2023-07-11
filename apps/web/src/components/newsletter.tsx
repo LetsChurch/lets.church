2c5cc2cb7a320dc91fd5729b8b036303cad10ba5
Ken Powers <ken@lets.church>
2023-07-11 06:25:46 -0400

    Fix newsletter form not submitting email address

--- apps/web/src/components/newsletter.tsx
+++ apps/web/src/components/newsletter.tsx
@@ -66,18 +66,13 @@ export default function Newsletter() {
               Email address
             </label>
             <input
-              id="email-address"
               name="email"
               type="email"
               autocomplete="email"
               required
               class="min-w-0 flex-auto rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              classList={{
-                'bg-gray-100': Boolean(email()),
-              }}
               placeholder="Enter your email"
               value={email()}
-              disabled={Boolean(email())}
             />
             <Button type="submit" disabled={submitting.pending}>
               Subscribe
