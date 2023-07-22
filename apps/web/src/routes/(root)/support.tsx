ba650b0e5973545a45d7545d97324cbaa3ab2482
Ken Powers <ken@lets.church>
2023-07-22 06:16:50 -0400

    Replace Donorbox with Zeffy

--- apps/web/src/routes/(root)/support.tsx
+++ apps/web/src/routes/(root)/support.tsx
@@ -2,15 +2,14 @@ import A from '~/components/content/a';
 import H1 from '~/components/content/h1';
 import H2 from '~/components/content/h2';
 import P from '~/components/content/p';
-import { Donorbox } from '~/components/donorbox';
+import { Button } from '~/components/form';
 
 export default function SupportRoute() {
   return (
-    <div class="px-6 py-3 lg:px-8">
-      <div class="mx-auto flex max-w-3xl flex-col text-base leading-7 text-gray-700 md:block">
-        <Donorbox class="order-3 mb-6 mt-6 flex items-center justify-center md:float-right md:ml-6 md:mt-0" />
-        <H1 class="order-0">Support Let's Church</H1>
-        <P class="order-1">
+    <div class="bg-white px-6 py-3 lg:px-8">
+      <div class="mx-auto max-w-3xl text-base leading-7 text-gray-700">
+        <H1>Support Let's Church</H1>
+        <P>
           <A href="/about">
             Let's Church is a 501(c)(3) non-profit organization
           </A>
@@ -21,14 +20,23 @@ export default function SupportRoute() {
           around the world. All donations are tax-deductible in the United
           States.
         </P>
-        <P class="order-2">
+        <P>
+          <form
+            action="https://www.zeffy.com/en-US/donation-form/5da9e1c3-a8e2-4bb4-817a-5dbbb968ec6b"
+            class="flex items-center justify-center"
+            target="_blank"
+          >
+            <Button type="submit">Donate to Let's Church</Button>
+          </form>
+        </P>
+        <P>
           Please do not let any giving to Let's Church interfere with giving to
           your local church. It is important that you first support your local
           community of believers before prayerfully considering how much you can
           give to Let's Church.
         </P>
-        <H2 class="order-4">Spread the Word</H2>
-        <P class="order-5">
+        <H2>Spread the Word</H2>
+        <P>
           Another way you can support Let's Church is by spreading the word.
           Share our website, share our social media, share sermons, videos, and
           podcasts that have been helpful or edifying to you. If you know of any
