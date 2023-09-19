cb8a55a8cce01a3714629ec87361233a6860da20
Ken Powers <ken@lets.church>
2023-09-18 20:17:03 -0400

    Parallelize reindex script

--- services/gateway/scripts/reindex.ts
+++ services/gateway/scripts/reindex.ts
@@ -1,5 +1,6 @@
 import { select } from '@inquirer/prompts';
 import ora from 'ora';
+import pMap from 'p-map';
 import { client } from '../src/temporal';
 import { BACKGROUND_QUEUE } from '../src/temporal/queues';
 import { emptySignal } from '../src/temporal/signals';
@@ -31,33 +32,37 @@ let queued = 0;
 
 const spinner = ora(`Queueing ${total} uploads`).start();
 
-for (const { id } of uploads) {
-  if (what === 'transcripts' || what === 'all') {
-    await (
-      await client
-    ).workflow.signalWithStart(indexDocumentWorkflow, {
-      taskQueue: BACKGROUND_QUEUE,
-      workflowId: `reindexTranscript:${id}`,
-      args: ['transcript', id, `${id}/transcript.vtt`],
-      signal: emptySignal,
-      signalArgs: [],
-    });
-  }
+await pMap(
+  uploads,
+  async ({ id }) => {
+    if (what === 'transcripts' || what === 'all') {
+      await (
+        await client
+      ).workflow.signalWithStart(indexDocumentWorkflow, {
+        taskQueue: BACKGROUND_QUEUE,
+        workflowId: `reindexTranscript:${id}`,
+        args: ['transcript', id, `${id}/transcript.vtt`],
+        signal: emptySignal,
+        signalArgs: [],
+      });
+    }
 
-  if (what === 'uploads' || what === 'all') {
-    await (
-      await client
-    ).workflow.signalWithStart(indexDocumentWorkflow, {
-      taskQueue: BACKGROUND_QUEUE,
-      workflowId: `reindexUpload:${id}`,
-      args: ['upload', id],
-      signal: emptySignal,
-      signalArgs: [],
-    });
-  }
+    if (what === 'uploads' || what === 'all') {
+      await (
+        await client
+      ).workflow.signalWithStart(indexDocumentWorkflow, {
+        taskQueue: BACKGROUND_QUEUE,
+        workflowId: `reindexUpload:${id}`,
+        args: ['upload', id],
+        signal: emptySignal,
+        signalArgs: [],
+      });
+    }
 
-  queued += 1;
-  spinner.text = `Queued ${queued}/${total} uploads`;
-}
+    queued += 1;
+    spinner.text = `Queued ${queued}/${total} uploads`;
+  },
+  { concurrency: 15 },
+);
 
 spinner.succeed('Done!');
