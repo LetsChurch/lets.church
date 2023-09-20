b64559ffd2fff4d5e164f3d4bcb11638b4afcd46
Ken Powers <ken@lets.church>
2023-09-20 08:55:10 -0400

    Update delete uploads script

--- services/gateway/scripts/delete-uploads.ts
+++ services/gateway/scripts/delete-uploads.ts
@@ -1,28 +1,60 @@
-import { input, confirm } from '@inquirer/prompts';
+import { readFile } from 'node:fs/promises';
+import { input, confirm, select } from '@inquirer/prompts';
 import prisma from '../src/util/prisma';
 import { client } from '../src/temporal';
 import { BACKGROUND_QUEUE } from '../src/temporal/queues';
 import { deleteUploadWorkflow } from '../src/temporal/workflows';
 
-const slug = await input({ message: 'channel slug:' });
-
-const recs = await prisma.uploadRecord.findMany({
-  where: { channel: { slug } },
-  select: { id: true, title: true },
-  take: Number.MAX_SAFE_INTEGER,
+const source = await select({
+  message: 'What do you want to delete?',
+  choices: [
+    { name: 'List of IDs', value: 'ids' },
+    { name: 'Uploads from Channel Slug', value: 'slug' },
+  ],
 });
+
+let ids: Array<string> = [];
+
+if (source === 'slug') {
+  const slug = await input({ message: 'channel slug:' });
+
+  const recs = await prisma.uploadRecord.findMany({
+    where: { channel: { slug } },
+    select: { id: true, title: true },
+    take: Number.MAX_SAFE_INTEGER,
+  });
+
+  ids = recs.map((r) => r.id);
+} else if (source === 'ids') {
+  const subSource = await select({
+    message: 'IDs from:',
+    choices: [
+      { name: 'File', value: 'file' },
+      { name: 'Input', value: 'input' },
+    ],
+  });
+  if (subSource === 'file') {
+    const filename = await input({ message: 'File name:' });
+    const text = await readFile(filename, 'utf-8');
+    ids = text.trim().split(/\n/g);
+  } else {
+    ids = (await input({ message: 'ids (comma-separated):' }))
+      .split(/,/g)
+      .map((s) => s.trim());
+  }
+}
 
 if (
   !(await confirm({
-    message: `Delete ${recs.length} upload records?`,
+    message: `Delete ${ids.length} upload records?`,
     default: false,
   }))
 ) {
   process.exit(0);
 }
 
-for (const { id, title } of recs) {
-  console.log(`Queueing ${id} for deletion: ${title}`);
+for (const id of ids) {
+  console.log(`Queueing ${id} for deletion`);
 
   await (
     await client
@@ -33,4 +65,4 @@ for (const { id, title } of recs) {
   });
 }
 
-console.log(`${recs.length} uploads queued for deletion!`);
+console.log(`${ids.length} uploads queued for deletion!`);
