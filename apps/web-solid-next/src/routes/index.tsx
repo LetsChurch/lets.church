aca4313ed99fef3a9d2f6ef1b789a44fe990ac0f
Ken Powers <ken@lets.church>
2024-01-17 21:04:31 -0500

    Initialize new solid-start project for upgrade

---
+++ apps/web-solid-next/src/routes/index.tsx
@@ -0,0 +1,31 @@
+import { A } from '@solidjs/router';
+import Counter from '~/components/Counter';
+
+export default function Home() {
+  return (
+    <main class="mx-auto p-4 text-center text-gray-700">
+      <h1 class="max-6-xs my-16 text-6xl font-thin uppercase text-sky-700">
+        Hello world!
+      </h1>
+      <Counter />
+      <p class="mt-8">
+        Visit{' '}
+        <a
+          href="https://solidjs.com"
+          target="_blank"
+          class="text-sky-600 hover:underline"
+        >
+          solidjs.com
+        </a>{' '}
+        to learn how to build Solid apps.
+      </p>
+      <p class="my-4">
+        <span>Home</span>
+        {' - '}
+        <A href="/about" class="text-sky-600 hover:underline">
+          About Page
+        </A>{' '}
+      </p>
+    </main>
+  );
+}
