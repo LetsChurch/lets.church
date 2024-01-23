8604f6e5c86b27506e98fbcccc36b1a4129a75e6
Ken Powers <ken@lets.church>
2024-01-22 23:12:39 -0500

    Port tailwind config

---
+++ apps/web-solid-next/tailwind.config.mjs
@@ -0,0 +1,17 @@
+import forms from '@tailwindcss/forms';
+
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      height: {
+        screen: ['100vh', '100dvh'],
+      },
+      transitionTimingFunction: {
+        'in-expo': 'cubic-bezier(0.95, 0.05, 0.795, 0.035)',
+        'out-expo': 'cubic-bezier(0.19, 1, 0.22, 1)',
+      },
+    },
+  },
+  plugins: [forms],
+};
