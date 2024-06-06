a1f98a0ba6643886c216b5dc63628d5ddf837ce5
Ken Powers <ken@lets.church>
2024-06-05 21:28:06 -0400

    Update create user script to use default password

--- services/gateway/scripts/create-user.ts
+++ services/gateway/scripts/create-user.ts
@@ -7,6 +7,7 @@ import {
 import { z } from 'zod';
 import argon2 from 'argon2';
 import short from 'short-uuid';
+import { faker } from '@faker-js/faker';
 import prisma from '../src/util/prisma';
 import logger from '../src/util/logger';
 
@@ -15,10 +16,17 @@ const email = await input({
   message: 'Email:',
   validate: (val) => z.string().email().safeParse(val).success,
 });
-const password = await argon2.hash(
-  await passwordInput({ message: 'Password:' }),
-  { type: argon2.argon2id },
-);
+
+let printPassword = false;
+let password = await passwordInput({
+  message: 'Password:',
+});
+
+if (!password) {
+  password = faker.internet.password({ length: 20, memorable: true });
+  printPassword = true;
+}
+
 const role = await select({
   message: 'Role:',
   choices: [
@@ -39,7 +47,7 @@ if (
 const user = await prisma.appUser.create({
   data: {
     username,
-    password,
+    password: await argon2.hash(password, { type: argon2.argon2id }),
     role: z.enum(['USER', 'ADMIN'] as const).parse(role),
     emails: {
       create: { email, verifiedAt: new Date() },
@@ -52,3 +60,7 @@ const translator = short();
 logger.info('User created!');
 logger.info(user.id);
 logger.info(translator.fromUUID(user.id));
+
+if (printPassword) {
+  logger.info({ password });
+}
