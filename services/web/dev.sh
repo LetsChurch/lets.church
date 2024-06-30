b4052af72f5cac9b403e32b472f9756dee3dde00
Ken Powers <ken@lets.church>
2024-06-29 23:34:54 -0400

    Build out header in web-go

--- services/web/dev.sh
+++ services/web/dev.sh
@@ -10,9 +10,9 @@ templ generate --watch -v --proxy="http://localhost:3000" --proxybind="0.0.0.0" 
 npx esbuild app/index.ts --bundle --format=esm --outdir=assets --watch=forever &
 
 # css
-watchexec --watch app --exts css -- "npx lightningcss-cli --minify --bundle --targets 'last 2 versions' app/index.css -o assets/styles.css" &
+watchexec --watch app --exts css -- "npx lightningcss-cli --minify --bundle --custom-media --targets 'last 2 versions and not dead' app/index.css -o assets/styles.css" &
 
 # built assets
-watchexec --watch assets -- templ generate --notify-proxy --proxybind=0.0.0.0 --proxyport=3001 &
+watchexec --watch assets --no-vcs-ignore -- templ generate --notify-proxy --proxybind=0.0.0.0 --proxyport=3001 &
 
 wait
