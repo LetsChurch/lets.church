278f719b980752a8bec1fafb691580f1a9bee0fa
Ken Powers <ken@lets.church>
2024-08-22 23:08:24 -0400

    Extract concurrent task and activity executions for import worker to environment

--- services/gateway/src/temporal/workers/import-worker.ts
+++ services/gateway/src/temporal/workers/import-worker.ts
@@ -16,14 +16,27 @@ await checkYtDlp();
 
 const TEMPORAL_ADDRESS = envariant('TEMPORAL_ADDRESS');
 
+const MAX_CONCURRENT_WORKFLOW_TASK_EXECUTIONS = envariant(
+  'MAX_CONCURRENT_WORKFLOW_TASK_EXECUTIONS',
+);
+const MAX_CONCURRENT_ACTIVITY_TASK_EXECUTIONS = envariant(
+  'MAX_CONCURRENT_ACTIVITY_TASK_EXECUTIONS',
+);
+
 const importWorker = await Worker.create({
   identity: `import-worker ${envariant('IDENTITY')}`,
   connection: await NativeConnection.connect({ address: TEMPORAL_ADDRESS }),
   activities: importActivities,
   taskQueue: IMPORT_QUEUE,
   shutdownGraceTime: envariant('TEMPORAL_SHUTDOWN_GRACE_TIME'),
-  maxConcurrentWorkflowTaskExecutions: 2,
-  maxConcurrentActivityTaskExecutions: 2,
+  maxConcurrentWorkflowTaskExecutions: parseInt(
+    MAX_CONCURRENT_WORKFLOW_TASK_EXECUTIONS,
+    10,
+  ),
+  maxConcurrentActivityTaskExecutions: parseInt(
+    MAX_CONCURRENT_ACTIVITY_TASK_EXECUTIONS,
+    10,
+  ),
 });
 
 await importWorker.run();
