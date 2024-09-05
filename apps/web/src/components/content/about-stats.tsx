618f0564001e40ebe354770fcbc47e7c99a2c2e4
Ken Powers <ken@lets.church>
2024-09-04 22:28:31 -0400

    Update about page

--- apps/web/src/components/content/about-stats.tsx
+++ apps/web/src/components/content/about-stats.tsx
@@ -44,7 +44,7 @@ export default function AboutStats() {
           </dd>
         </div>
         <div class="mx-auto flex max-w-xs flex-col gap-y-4">
-          <dt class="text-base leading-7 text-gray-600">No Cost</dt>
+          <dt class="text-base leading-7 text-gray-600">No Cost To You</dt>
           <dd class="order-first px-0 text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
             $0
           </dd>
