4885e736b1a25a0c08082f2af3a215c8f6584410
Ken Powers <ken@lets.church>
2024-12-12 17:03:25 -0500

    Make player clientOnly

--- apps/web-next/src/components/media/player/index.ts
+++ apps/web-next/src/components/media/player/index.ts
@@ -1,1 +1,7 @@
-export { default } from './player';
+import { clientOnly } from '@solidjs/start';
+
+const Player = clientOnly(async () => {
+  return import('./player');
+});
+
+export default Player;
