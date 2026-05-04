d053c6ed7a579efe55f64cf5b2c216c9621128b2
Ken Powers <ken@lets.church>
2026-05-04 05:54:02 -0400

    Upgrade to postgreSQL 18

--- etc/postgres-init/listmonk.sql
+++ etc/postgres-init/listmonk.sql
@@ -1,3 +1,5 @@
 CREATE USER listmonk WITH PASSWORD 'listmonk';
 CREATE DATABASE listmonk;
 GRANT ALL PRIVILEGES ON DATABASE listmonk TO listmonk;
+\c listmonk
+GRANT ALL ON SCHEMA public TO listmonk;
--- etc/postgres-init/temporal.sql
+++ etc/postgres-init/temporal.sql
@@ -3,3 +3,7 @@ CREATE DATABASE temporal;
 CREATE DATABASE temporal_visibility;
 GRANT ALL PRIVILEGES ON DATABASE temporal TO temporal;
 GRANT ALL PRIVILEGES ON DATABASE temporal_visibility TO temporal;
+\c temporal
+GRANT ALL ON SCHEMA public TO temporal;
+\c temporal_visibility
+GRANT ALL ON SCHEMA public TO temporal;
